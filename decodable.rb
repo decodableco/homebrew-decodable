4549dc4f8d9ee49898ac66a20a07d2bcddeda39b
Jared Breeden <jbreeden@decodable.co>
2023-10-06 10:50:23 -0400

    #3: Upgrade CLI to version 1.8.0

--- decodable.rb
+++ decodable.rb
@@ -4,20 +4,20 @@
 class Decodable < Formula
   desc "Manage Decodable resources from the CLI"
   homepage "https://www.decodable.co/"
-  version "1.7.0"
+  version "1.8.0"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://releases.decodable.co/decodable-cli/darwin/amd64/decodable-cli-darwin-amd64-1.7.0.tar.gz"
-      sha256 "3334a1843774a2572ff237c20949894bc41364351775d0ef468faf25de90f38b"
+      url "https://releases.decodable.co/decodable-cli/darwin/amd64/decodable-cli-darwin-amd64-1.8.0.tar.gz"
+      sha256 "0c20d11fdfdc5362230637e9f4878c5d327cd3b8f44983d697b83c6d26e39d77"
 
       def install
         bin.install "bin/decodable"
       end
     end
     if Hardware::CPU.arm?
-      url "https://releases.decodable.co/decodable-cli/darwin/arm64/decodable-cli-darwin-arm64-1.7.0.tar.gz"
-      sha256 "3a789d06dfd7750684bc133ea9bc5954eb576e84dc940da664ee9126cc2cb102"
+      url "https://releases.decodable.co/decodable-cli/darwin/arm64/decodable-cli-darwin-arm64-1.8.0.tar.gz"
+      sha256 "b8f8ba58b41a8a004b8d18889973550e814750a1fc16f4ebc6831edcd2713031"
 
       def install
         bin.install "bin/decodable"
