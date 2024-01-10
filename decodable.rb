9b7dd5c557332aa5be176b55f1db1258b99ac382
Robert Metzger <89049+rmetzger@users.noreply.github.com>
2024-01-10 14:36:01 +0100

    DE-5818: Bump CLI from 1.8.1 to 1.9.0 (#6)

--- decodable.rb
+++ decodable.rb
@@ -4,20 +4,20 @@
 class Decodable < Formula
   desc "Manage Decodable resources from the CLI"
   homepage "https://www.decodable.co/"
-  version "1.8.1"
+  version "1.9.0"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://releases.decodable.co/decodable-cli/darwin/amd64/decodable-cli-darwin-amd64-1.8.1.tar.gz"
-      sha256 "f1fba167283ba298f8ae582ab88252dbf523069b9f84e63ac8884e9845fcc6a2"
+      url "https://releases.decodable.co/decodable-cli/darwin/amd64/decodable-cli-darwin-amd64-1.9.0.tar.gz"
+      sha256 "289eb1c7b9a5ddd3cae4c4c0de3a2fdcdce20e905c1a26d1c297d570e00e3d2a"
 
       def install
         bin.install "bin/decodable"
       end
     end
     if Hardware::CPU.arm?
-      url "https://releases.decodable.co/decodable-cli/darwin/arm64/decodable-cli-darwin-arm64-1.8.1.tar.gz"
-      sha256 "aff3dd4b29627b775a70989c52458b27eef16e0b4eeea2d26e92c313bcc05991"
+      url "https://releases.decodable.co/decodable-cli/darwin/arm64/decodable-cli-darwin-arm64-1.9.0.tar.gz"
+      sha256 "846bced3abee81aae256d39dc2ed6a181bca72fa6b388f33dd687b9df5b588d4"
 
       def install
         bin.install "bin/decodable"
