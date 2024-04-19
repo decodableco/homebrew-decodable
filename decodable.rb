49d8dddb03d93799cacf8d87f0befa251d70eeaf
Timothy James <tim@decodable.co>
2024-04-19 10:55:33 -0700

    DE-6500: Release CLI 1.14.0

--- decodable.rb
+++ decodable.rb
@@ -4,20 +4,20 @@
 class Decodable < Formula
   desc "Manage Decodable resources from the CLI"
   homepage "https://www.decodable.co/"
-  version "1.13.0"
+  version "1.14.0"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://releases.decodable.co/decodable-cli/darwin/amd64/decodable-cli-darwin-amd64-1.13.0.tar.gz"
-      sha256 "fa9ca67f90e6f3aae932538540aa49b50975cd0d1b1b707dac075fd19d60f3c9"
+      url "https://releases.decodable.co/decodable-cli/darwin/amd64/decodable-cli-darwin-amd64-1.14.0.tar.gz"
+      sha256 "97d550d5dd3547c508b73deba5b46431d6034705fcd5e23f680d5c594d3e853c"
 
       def install
         bin.install "bin/decodable"
       end
     end
     if Hardware::CPU.arm?
-      url "https://releases.decodable.co/decodable-cli/darwin/arm64/decodable-cli-darwin-arm64-1.13.0.tar.gz"
-      sha256 "13d68cb4583755e6cddf096d1454c82a061c35f9fd22b97f687e5a911bc44200"
+      url "https://releases.decodable.co/decodable-cli/darwin/arm64/decodable-cli-darwin-arm64-1.14.0.tar.gz"
+      sha256 "5c80cf1bbea8860655a2a59c95aeba08cd5ee6182a68e013a96e9d50e7ee3b2f"
 
       def install
         bin.install "bin/decodable"
